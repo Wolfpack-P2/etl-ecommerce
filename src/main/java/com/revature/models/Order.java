d5d150efbc0b9590434c22c27408662664a1e2d6
jlcrogue <jlcrogue@gmail.com>
2022-02-21 14:32:23 -0700

    added order model

--- src/main/java/com/revature/models/Order.java
+++ src/main/java/com/revature/models/Order.java
@@ -1,4 +1,33 @@
 package com.revature.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table
+@NoArgsConstructor @AllArgsConstructor @Data
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer order_id;
+    private Integer customer_id;
+    private String customer_name;
+    private Integer product_id;
+    private String product_name;
+    private String product_category;
+    private String payment_type;
+    private Integer qty;
+    private Double price;
+    private Timestamp datetime;
+    private String country;
+    private String city;
+    private String ecommerce_website_name;
+    private Character payment_txn_success;
+    private String failure_reason;
 }
