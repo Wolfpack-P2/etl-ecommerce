d8649247ec7d6eb92525b4ffcc916a6767f34217
jlcrogue <jlcrogue@gmail.com>
2022-02-22 08:36:25 -0700

    added annotations to EtlCommerceApplication

--- src/main/java/com/revature/etlecommerce/EtlECommerceApplication.java
+++ src/main/java/com/revature/etlecommerce/EtlECommerceApplication.java
@@ -2,8 +2,16 @@ package com.revature.etlecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.revature.repositories")
+@EntityScan("com.revature.models")
+@ComponentScan("com.revature")
+@EnableTransactionManagement
 public class EtlECommerceApplication {
 
     public static void main(String[] args) {
