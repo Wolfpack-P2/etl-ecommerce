f4f8cabf431f235cdf1509eb8a2e834d8c8548e3
jamesabenj <james.abenj@gmail.com>
2022-02-24 15:51:45 -0500

    Add login function

--- src/main/resources/webapp/scripts/index.js
+++ src/main/resources/webapp/scripts/index.js
@@ -2,8 +2,10 @@ function $(x){
     return document.getElementById(x)
 }
 
+let url = "http://localhost:8080/ETL-E-Commerce/login"
 
 
+let user = {}
 
 function showRegister() {
     let registerCard = $("register-card")
@@ -13,10 +15,21 @@ function showRegister() {
     registerCard.style.display = "block"
 }
 
-/* function showLogin() {
-    let registerCard = $("register-card")
-    let loginCard = $("login-card")
-    console.log(loginCard)
-    registerCard.parentNode.replaceChild(loginCard, registerCard)
-} */
+function login() {
+  console.log("inside login method!")
+    let username = $("username").value
+    let password = $("password").value
+    let user = {
+      "username": username,
+      "password": password
+    }
+    let response = fetch(url, {
+                   method: 'post',
+                   body: JSON.stringify(user)
+                 }).then(function(response) {
+                   return response.json();
+                 })
+    console.log(response)
+
+}
 
