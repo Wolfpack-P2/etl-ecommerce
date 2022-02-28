20a2dc47bbc6f7e0436ffc77d47bf3bc00e53a4f
Yasin Yardimoglu <yasin.yardimoglu@revature.net>
2022-02-28 02:03:29 -0600

    added method to register user

--- src/main/java/com/revature/controllers/RegisterController.java
+++ src/main/java/com/revature/controllers/RegisterController.java
@@ -1,7 +1,25 @@
 package com.revature.controllers;
 
+import com.revature.models.User;
+import com.revature.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/users")
 @RestController
 public class RegisterController {
+
+    private UserRepository userRepo;
+    @Autowired
+    public RegisterController(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @PostMapping("/register")
+    public void registerUser(@RequestBody User user) {
+        userRepo.registerUser(user.getFirstName(), user.getLastName(), user.getUsername(), user.getPassword());
+    }
 }
