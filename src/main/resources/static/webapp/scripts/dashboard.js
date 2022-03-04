421ff6fa435f76e04e0714553a9bdded6651616f
jamesabenj <james.abenj@gmail.com>
2022-03-04 06:14:09 -0500

    adding graphs

--- src/main/resources/static/webapp/scripts/dashboard.js
+++ src/main/resources/static/webapp/scripts/dashboard.js
@@ -2,12 +2,7 @@ function $(x) {
     return document.getElementById(x)
 }
 
-function fetchUser() {
-    let header = $("dashboard-heading")
-    header.innerHTML = `Dashboard`
-}
-
-
+let catUrl="http://localhost:8080/ETL-E-Commerce/order/category"
 /* function drawChart() {
     // Define the chart to be drawn.
     var data = new google.visualization.DataTable();
@@ -28,17 +23,133 @@ function fetchUser() {
     chart.draw(data, options);
   } */
 
+  function drawDataComp() {
+    let chartData = new google.visualization.arrayToDataTable([
+      ['Data', 'Clean', 'Dirty'],
+      [1000, 732, 268]
+    ])
+
+    let chart = new google.visualization.BarChart(document.getElementById('bar-div'));
+    let options = {
+      bars: 'horizontal',
+      width: '900',
+      backgroundColor: '#363c4d',
+      hAxis: {
+        textStyle: {
+          color: '#ffffff'
+        }
+      },
+      vAxis: {
+        textStyle: {
+          color: '#ffffff'
+        }
+      },
+      legend: {
+        textStyle: {
+          color: '#ffffff'
+        }
+      }
+    }
+
+    chart.draw(chartData, options)
+  }
+
+  async function drawColumnChart() {
+
+    let chartData = new google.visualization.DataTable();
+    chartData.addColumn('string', 'Product Category');
+    chartData.addColumn('number', 'Order Volume');
+
+    await fetch(catUrl)
+    .then((resp) => {
+      return resp.json()
+    })
+    .then(data => {
+      data.forEach(element => {
+        chartData.addRow([element.product_category, element.count])
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+                      
+    var chart = new google.visualization.ColumnChart(document.getElementById('column-div'));
+    
+    let options = {
+      title: "Order Volume per Product Category",
+      titleTextStyle: {
+        color: '#ffffff'
+      },
+      backgroundColor: '#363c4d',
+      width: '900',
+      height: '450',
+      hAxis: {
+        textStyle: {
+          color: '#ffffff'
+        }
+      },
+      vAxis: {
+        textStyle: {
+          color: '#ffffff'
+        }
+      },
+      legend: {
+        textStyle: {
+          color: '#ffffff'
+        }
+      },
+      annotations: {
+        textStyle: {
+          fontSize: 10
+        }
+      }
+    }
+
+    chart.draw(chartData, options);
+
+
+  }
+
+  async function drawTable() {
+    await fetch(catUrl)
+    .then(resp => {
+      return resp.json()
+    })
+    .then(data => {
+      data.forEach(element => {
+        let tr = document.createElement('tr')
+        let th = document.createElement('th')
+        th.innerHTML = element.product_category
+        let td = document.createElement('td')
+        td.innerHTML = element.count
+        tr.append(th)
+        tr.append(td)
+        $('category-table').append(tr)
+      })
+    })
+  }
 
   async function drawRegionsMap() {
-    var data = google.visualization.arrayToDataTable([
-      ['Country', 'Category', "Sales"],
-      ['Germany', "Snitzel", 1000],
-      ['United States', "Guns", 300],
-      ['Brazil', "Coconuts", 400],
-      ['Canada', "Maple Syrup", 500],
-      ['France', "Baguets", 600],
-      ['RU', "Violence", 700]
-    ]);
+
+
+    let chartData = new google.visualization.DataTable();
+    chartData.addColumn('string', 'Country');
+    chartData.addColumn('string', 'Product Category');
+    chartData.addColumn('number', 'Order Volume');
+      
+
+    await fetch ('http://localhost:8080/ETL-E-Commerce/order/category/country')
+    .then(resp => {
+      return resp.json()
+    })
+    .then(data => {
+      data.forEach(element => {
+        chartData.addRow([element.country, element.product_category, element.count])
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
 
     var options = {
         backgroundColor: '#363c4d',
@@ -48,11 +159,6 @@ function fetchUser() {
 
     var chart = new google.visualization.GeoChart(document.getElementById('regions-div'));
 
-    chart.draw(data, options);
+    chart.draw(chartData, options);
   }
 
-    /* let geoTab = $("nav-geochart")
-    console.log(geoTab)
-    geoTab.click(function(){
-      $(this).tab('show');
-    }) */
