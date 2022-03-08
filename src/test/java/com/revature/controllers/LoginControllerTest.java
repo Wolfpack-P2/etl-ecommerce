9b16bbb9b961d6144e1b7f19a552e7b911ad19db
RealmsEnd <chriscrawford360@outlook.com>
2022-03-08 17:47:23 -0600

    Updated Order Model Variable Names

---
+++ src/test/java/com/revature/controllers/LoginControllerTest.java
@@ -0,0 +1,15 @@
+package com.revature.controllers;
+
+import org.junit.Test;
+
+public class LoginControllerTest {
+    @Test
+    public void testGetUsers() {
+
+    }
+
+    @Test
+    public void testResponse() {
+
+    }
+}
