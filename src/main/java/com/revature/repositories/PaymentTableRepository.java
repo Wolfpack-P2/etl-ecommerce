b55529e46bc47a4db543a974f97a387500d1edd9
colleenl <77894664+colleenl@users.noreply.github.com>
2022-03-10 12:13:39 -0500

    Revert "working back end with basic get all"

--- src/main/java/com/revature/repositories/PaymentTableRepository.java
+++
@@ -1,8 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.models.PayementEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentTableRepository extends JpaRepository<PayementEntity,Integer> {
-
-}
