cb8e330bfc45b93dd2303cd3a560ae867da8803e
usercourtney <1998courtneyh@gmail.com>
2022-03-17 23:00:40 -0500

    functioning research table?

--- src/main/resources/static/webapp/scripts/research.js
+++ src/main/resources/static/webapp/scripts/research.js
@@ -7,6 +7,91 @@
 // Table
 // How much does each customer spend on average Per Country?
 // Extra question: What is the average quantity of items purchased per transaction? per country?
+$(document).ready(function () {
+    getAllResearch();
+});
+
+function getAllResearch(){
+    let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                let response = JSON.parse(xhr.responseText);
+                console.log(response);
+            let x = document.getElementById("researchTable");
+            x.remove();
+
+            let table = document.createElement("table");
+            table.setAttribute("id", "researchTable");
+            table.setAttribute("class", "table table-striped table-bordered table-sort sortable");
+            let thead = document.createElement("thead");
+            thead.setAttribute("id", "thead");
+            let tbody = document.createElement("tbody");
+            tbody.setAttribute("id", "tbody");
+            table.appendChild(thead);
+            table.appendChild(tbody);
+            document.getElementById("researchDiv").appendChild(table);
+            // Create table header array
+            let header = [
+                "Country",
+                "Average Sale Per Transaction",
+                "Average Quantity of Items Per Sale"
+            ];
+            // Create table header row
+            let headerRow = document.createElement("tr");
+            header.forEach(function (header) {
+                let th = document.createElement("th");
+                th.setAttribute("style", "cursor: pointer");
+                th.innerHTML = header;
+                headerRow.appendChild(th);
+            });
+            thead.appendChild(headerRow);
+
+            let researchArray = [];
+            for (let i = 0; i < response.length; i++) {
+                let research = response[i];
+                researchArray.push(research);
+            }
+            let countries = [];
+            let averageSale = [];
+            let averageQty = [];
+            for(let i = 0; i < researchArray.length; i++){
+                if (countries.indexOf(researchArray[i].country) == -1) {
+                    countries.push(researchArray[i].country);
+                    averageSale.push(0);
+                    averageQty.push(0);
+
+                    let countryIndex = countries.indexOf(researchArray[i].country);
+                    if (researchArray[i].paymentTxnSuccess == "Y"){
+                        averageSale[countryIndex] += researchArray[i].totalPrice;
+                        averageQty[countryIndex] += researchArray[i].qty;
+                    }
+                }
+           }
+            for (let i = 0; i < countries.length; i++) {
+                let row = document.createElement("tr");
+                let country = document.createElement("td");
+                country.innerHTML = countries[i];
+                row.appendChild(country);
+
+                let sale = document.createElement("td");
+                sale.innerHTML = averageSale[i];
+                row.appendChild(sale);
+
+                let qty = document.createElement("td");
+                qty.innerHTML = averageQty[i];
+                row.appendChild(qty);
+
+                table.append(row);
+            }
+
+//            sorttable.makeSortable(table);
+
+        }
+    }
+    xhr.open("GET", "http://localhost:8080/ETL-E-Commerce/research", true);
+    xhr.send();
+    getDayBarChart();
+}
 
 function getDayBarChart(){
     let chartStatus = Chart.getChart("barChart");
@@ -41,7 +126,6 @@ function getDayBarChart(){
                 }
             }
             let ctx = document.getElementById('barChart').getContext('2d');
-
             let myChart = new Chart(ctx, {
                 type: 'bar',
                 data: {
