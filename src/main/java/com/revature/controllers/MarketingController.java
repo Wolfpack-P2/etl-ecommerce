5c6f3b2c6d9e65e6ffa3a5713ae86c2f187b269d
jake ramsey <jacobramsey31@gmail.com>
2022-03-21 12:59:49 -0400

    line chart working now just need to make minor touch ups

--- src/main/java/com/revature/controllers/MarketingController.java
+++ src/main/java/com/revature/controllers/MarketingController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import com.revature.models.Marketing;
 import com.revature.repositories.MarketingRepository;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/marketing")
@@ -16,8 +19,9 @@ public class MarketingController {
     @Autowired
     MarketingRepository marketingRepository;
 
-    @RequestMapping
+    @GetMapping
     public List<Marketing> getAllMarkets() {
+        System.out.println(marketingRepository.findAll());
         return marketingRepository.findAll();
     }
     
