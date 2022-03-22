61dfc7a7b84d85dc5f894ce3c8a54ea1890835ed
RealmsEnd <chriscrawford360@outlook.com>
2022-03-22 09:11:15 -0500

    Jacob Changes

--- src/main/java/com/revature/order/Order.java
+++ src/main/java/com/revature/order/Order.java
@@ -8,6 +8,7 @@ import java.util.Objects;
 @Entity
 @Table(name = "order_table")
 public class Order {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
@@ -28,19 +29,23 @@ public class Order {
     @Column(name="qty")
     private Integer qty;
 
+    @Column(name="payment_txn_id")
+    private Integer paymentTxnId;
+
     // No args constructor
     public Order() {
         super();
     }
 
     // All args constructor
-    public Order(Integer orderId, String productName, String ecomWeb, Timestamp orderDate, Double price, Integer qty) {
+    public Order(Integer orderId, String productName, String ecomWeb, Timestamp orderDate, Double price, Integer qty, Integer paymentTxnId) {
         this.orderId = orderId;
         this.productName = productName;
         this.ecomWeb = ecomWeb;
         this.orderDate = orderDate;
         this.price = price;
         this.qty = qty;
+        this.paymentTxnId = paymentTxnId;
     }
 
     // Getters and setters
@@ -92,6 +97,14 @@ public class Order {
         this.qty = qty;
     }
 
+    public Integer getPaymentTxnId() {
+        return paymentTxnId;
+    }
+
+    public void setPaymentTxnId(Integer paymentTxnId) {
+        this.paymentTxnId = paymentTxnId;
+    }
+
     @Override
     public String toString() {
         return "OrderEntity{" +
@@ -101,6 +114,7 @@ public class Order {
                 ", orderDate=" + orderDate +
                 ", price=" + price +
                 ", qty=" + qty +
+                ", paymentTxnId=" + paymentTxnId +
                 '}';
     }
 
@@ -109,11 +123,11 @@ public class Order {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Order that = (Order) o;
-        return Objects.equals(orderId, that.orderId) && Objects.equals(productName, that.productName) && Objects.equals(ecomWeb, that.ecomWeb) && Objects.equals(orderDate, that.orderDate) && Objects.equals(price, that.price) && Objects.equals(qty, that.qty);
+        return Objects.equals(orderId, that.orderId) && Objects.equals(productName, that.productName) && Objects.equals(ecomWeb, that.ecomWeb) && Objects.equals(orderDate, that.orderDate) && Objects.equals(price, that.price) && Objects.equals(qty, that.qty) && Objects.equals(paymentTxnId, that.paymentTxnId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(orderId, productName, ecomWeb, orderDate, price, qty);
+        return Objects.hash(orderId, productName, ecomWeb, orderDate, price, qty, paymentTxnId);
     }
 }
